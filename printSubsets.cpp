be1163dc502f97d4f3f47bfd61dbd6852ece5e5f
Rohit <agrawalrohit738@gmail.com>
2023-01-16 15:08:33 +0530

    added printSubsets prompt

--- printSubsets.cpp
+++ printSubsets.cpp
@@ -5,7 +5,16 @@ using namespace std;
 int main()
 {
     string s1 = "Rohit";
+    string s2 = "Aditi";
 
-    cout<<s1<<endl;
+    if(s1 == s2)
+    {
+        cout<<s2<<endl;
+    }
+    else{
+        cout<<"Still "<<s2<<endl;
+    }
+
+    cout<<s1<<" Wrong"<<endl;
     return 0;
 }
